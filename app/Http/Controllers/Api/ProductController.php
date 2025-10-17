207247fa78985fd75a7a3d381be3c5f7a34112b9
Sebastian <sebastian@pumo.digital>
2025-10-17 16:42:30 +0200

    Products Paginate

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends Controller
     // Alle Produkte
     public function index()
     {
-        return Product::with('category')->get();
+        return Product::with('category')->paginate(10);
     }
 
     // Einzelnes Produkt anzeigen
